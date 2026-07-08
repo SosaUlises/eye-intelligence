daf414acc7f24a1de66bfe435c132c0cfa848d2c
Ulises <ulipvp@gmail.com>
2026-07-08 13:09:04 -0300

    Creacion de proyecto Streamlit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="E&E Stock Intelligence",
+    page_icon="📊",
+    layout="wide"
+)
+
+
+st.title("E&E Stock Intelligence")
+st.caption("Sistema inteligente de análisis de ventas, stock y demanda para E&E Calzados.")
+
+st.info("Proyecto iniciado correctamente.")
